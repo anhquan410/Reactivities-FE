36d0f4b7b29bec107936264a7c2cb9d4773781ef
AnhQuanNguyen <quan.na4102001@gmail.com>
2026-01-13 20:24:59 +0700

    feat: Implement infinite scrolling in ActivityList component; integrate CircularProgress loader for loading state and enhance user experience by loading more activities as the user scrolls. Update rendering logic to accommodate new structure.

--- src/features/activities/dashboard/ActivityList.tsx
+++ src/features/activities/dashboard/ActivityList.tsx
@@ -1,31 +1,62 @@
-import { Box, Typography } from "@mui/material";
-import ActivityCard from "./ActivityCard";
 import { useActivities } from "@/libs/hooks/useActivities";
+import { Box, Typography, CircularProgress } from "@mui/material";
+import InfiniteScroll from "react-infinite-scroller";
+
+import ActivityCard from "./ActivityCard";
 
 export default function ActivityList() {
   const {
     activitiesGrouped,
     isPending,
-    // isFetching,
-    // fetchNextPage,
-    // hasNextPage,
-    // isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
   } = useActivities();
+
+  const handleLoadMore = () => {
+    if (hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  };
 
   if (isPending) return <Typography variant="h5">Loading...</Typography>;
 
-  if (!activitiesGrouped) {
-    return <Typography>No activities found</Typography>;
-  }
+  if (!activitiesGrouped)
+    return <Typography variant="h5">No activities found</Typography>;
+
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
-      {activitiesGrouped.pages.map((activities, index) => (
-        <Box key={index} display="flex" flexDirection="column" gap={3}>
-          {activities?.items?.map((activity) => (
-            <ActivityCard key={activity.id} activity={activity} />
-          ))}
+    <InfiniteScroll
+      pageStart={0}
+      loadMore={handleLoadMore}
+      hasMore={hasNextPage}
+      loader={
+        <Box
+          key="loader"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            py: 2,
+          }}
+        >
+          <CircularProgress size={24} />
+          <Typography variant="body2" sx={{ ml: 1 }}>
+            Loading more activities...
+          </Typography>
         </Box>
-      ))}
-    </Box>
+      }
+      useWindow={true}
+      threshold={250}
+    >
+      <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
+        {activitiesGrouped.pages.map((activities, index) => (
+          <Box key={index} display="flex" flexDirection="column" gap={3}>
+            {activities?.items?.map((activity) => (
+              <ActivityCard key={activity.id} activity={activity} />
+            ))}
+          </Box>
+        ))}
+      </Box>
+    </InfiniteScroll>
   );
 }
